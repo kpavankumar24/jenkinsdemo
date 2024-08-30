3dc17eb640da6ab8c35a4f70af068806bc13ce28
kpavankumar24 <149552127+kpavankumar24@users.noreply.github.com>
2024-08-30 00:06:12 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello everyone");
+        System.out.println("Hello thanks for visiting this place");
         
     }
 
